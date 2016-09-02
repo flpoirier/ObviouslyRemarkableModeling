53267133bf67a61b4c974696947b92a765e98d3e
Feather <featherpoirier@gmail.com>
2016-09-02 19:08:49 -0400

    Continued Phase 3

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,11 +10,11 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
@@ -22,28 +22,50 @@ class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
 
     default = {primary_key: :id,
-               class_name: name.capitalize,
-               foreign_key: (name + "_id").to_sym}
+               class_name: name.camelcase,
+               foreign_key: (name.to_s + "_id").to_sym}
 
     options = default.merge(options)
 
-    options.each do |k,v|
-      self.k = v
+    options.each do |attr_name, val|
+      self.send("#{attr_name}=", val)
     end
-    
+
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+
+    default = {primary_key: :id,
+               class_name: name.classify,
+               foreign_key: (self_class_name.downcase + "_id").to_sym}
+    options = default.merge(options)
+
+    options.each do |attr_name, val|
+      self.send("#{attr_name}=", val)
+    end
+
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+
+    # self.assoc_options[name] = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      options = self.class.options[name]
+
+      key_val = self.send(options.foreign_key)
+      options
+        .model_class
+        .where(options.primary_key => key_val)
+        .first
+    end
+
   end
 
   def has_many(name, options = {})

fce5b9c7888009160d372ad863f4e8b977554b43
Feather <featherpoirier@gmail.com>
2016-09-02 17:20:13 -0400

    Phase 2 Complete

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -1,12 +1,28 @@
 require_relative 'db_connection'
 require_relative '01_sql_object'
+require 'byebug'
 
 module Searchable
   def where(params)
-    # ...
+    vals = params.values
+    attributes = params.keys
+    attributes.map! { |attr| "#{attr.to_s} = ?" }
+    result = DBConnection.execute2(<<-SQL, *vals)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{attributes.join(" AND ")}
+      SQL
+    final = []
+    result[1..-1].each do |el|
+      final << self.new(el)
+    end
+    final
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
